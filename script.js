f2efb6f0a30cfb7e82ce208b645bb3e42cc8570c
JJ awesome <jalejc2015@gmail.com>
2024-04-28 16:54:35 -0400

    insercion de pantalla de inicio y opciones de display en menu

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ document.getElementById("led-off").addEventListener("click", function() {
         .then(response => console.log('LED apagado'))
         .catch(error => console.error('Error:', error));
 });
+
+document.getElementById("girarD").addEventListener("click", function() {
+    enviarTextoAlServidor();
+});
+
+document.getElementById("girarI").addEventListener("click", function() {
+    enviarTextoAlServidor();
+});
+
+function enviarTextoAlServidor() {
+    const textoIngresado = document.getElementById("texto-ingreso").value;
+
+    fetch('/texto', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ texto: textoIngresado })
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Texto enviado correctamente');
+        } else {
+            console.error('Error al enviar texto:', response.statusText);
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
